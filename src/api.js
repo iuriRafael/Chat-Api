c024b26162cc498814bf21cd225124556748fd72
Iuri <iuri-rdutra2@educar.rs.gov.br>
2023-05-10 14:49:42 -0300

    2 parte feita

---
+++ src/api.js
@@ -0,0 +1,19 @@
+let express = require("express");
+let app = express();
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+
+const router = express.Router();
+app.use('/', router.get('/', (req, res)=>{
+    res.status(200).send("<h1>API-CHAT</h1>")
+}));
+
+app.use("/", router.get("/sobre", (resq, res, next) => {
+    res.status(200).send({
+        "nome":"API - CHAT",
+        "versão": "0.1.0",
+        "autor": "Iuri Rafael"
+    })
+}));
+
+module.exports = app;
